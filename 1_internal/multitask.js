2c8d04bfe47b64f954d8e0b198be9f30de29625f
orchsik <orchsik@naver.com>
2022-03-24 21:04:35 +0900

    study internal of node

---
+++ 1_internal/multitask.js
@@ -0,0 +1,33 @@
+const https = require("https");
+const crypto = require("crypto");
+const fs = require("fs");
+
+const start = Date.now();
+
+function doRequest() {
+  https
+    .request("https://www.google.com", (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log(Date.now() - start);
+      });
+    })
+    .end();
+}
+
+function doHash() {
+  crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+    console.log("Hash:", Date.now() - start);
+  });
+}
+
+doRequest();
+
+fs.readFile("multitask.js", "utf-8", () => {
+  console.log("FS:", Date.now() - start);
+});
+
+doHash();
+doHash();
+doHash();
+doHash();
---
+++ 1_internal/threads.js
@@ -0,0 +1,34 @@
+// for unix
+// process.env.UV_THREADPOOL_SIZE = 5;
+// for windows
+// package.json > set UV_THREADPOOL_SIZE=5 & node threads.js
+
+const crypto = require("crypto");
+
+const start = Date.now();
+
+crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+  console.log("1:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+  console.log("2:", Date.now() - start);
+});
+
+// If Node were single threaded...
+// 1.crypto.pbkdf2 -> 2.crypto.pbkdf2
+// 1second + 1second = 2second
+// But Node is multiple threaded
+// => totally 1second
+
+crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+  console.log("3:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+  console.log("4:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 51, "sha512", () => {
+  console.log("5:", Date.now() - start);
+});
